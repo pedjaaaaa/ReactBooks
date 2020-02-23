7aff61d6f4bcb3ed8c17a74d952d4615c7846abb
Pedja <vpedja12@gmail.com>
2020-02-23 00:13:46 -0800

    Initiated app

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import ReactDOM from "react-dom";
 import './App.css';
+import axios from "axios";
+
+import "./App.css";
 
 function App() {
+
+  const [book, setBook] = useState("");
+  const [result, setResult] = useState([]);
+  const [apiKey, setApiKey] = useState("AIzaSyAmtvjY3E_Xw0cc20_vqTG4uuMy4RUFvMg");
+
+  function handleChange(event) {
+
+    const book = event.target.value;
+
+    setBook(book);
+
+  }
+
+  function handleSubmit(event) {
+
+    event.preventDefault();
+
+    console.log(book);
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Book Search App</h1>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <input
+            onChange={handleSubmit}
+            type="text"
+            className="form-control mt-10"
+            placeholder="Search for books"
+            autoComplete="off"
+          />
+        </div>
+        <button type="submit" className="btn btn-danger">Search</button>
+      </form>
     </div>
   );
 }
