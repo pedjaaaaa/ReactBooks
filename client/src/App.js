e5242da7299f68a40ef3697cfe360a89aff181d1
Pedja <vpedja12@gmail.com>
2020-02-25 17:40:34 -0800

    added api

--- client/src/App.js
+++ client/src/App.js
@@ -2,17 +2,14 @@ import React, { useState } from 'react';
 import ReactDOM from "react-dom";
 import './App.css';
 import axios from "axios";
-
 import "./App.css";
 
 function App() {
-
   const [book, setBook] = useState("");
   const [result, setResult] = useState([]);
   const [apiKey, setApiKey] = useState("AIzaSyAmtvjY3E_Xw0cc20_vqTG4uuMy4RUFvMg");
 
   function handleChange(event) {
-
     const book = event.target.value;
 
     setBook(book);
@@ -24,6 +21,12 @@ function App() {
     event.preventDefault();
 
     console.log(book);
+
+    axios.get("https://www.googleapis.com/books/v1/volumes?q=" + book + "&key=" + apiKey + "&maxResults=40")
+    .then(data => {
+      setResult(data.data.items)
+      console.log(data.data.items)
+    })
 
   }
 
@@ -42,6 +45,9 @@ function App() {
         </div>
         <button type="submit" className="btn btn-danger">Search</button>
       </form>
+      {result.map(book => (
+        <img src={book.volumeInfo.imageLinks.thumbnail} alt={book.title}/>
+      ))}
     </div>
   );
 }
